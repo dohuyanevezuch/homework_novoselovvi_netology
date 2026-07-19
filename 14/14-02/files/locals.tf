5ccd6b52cc95d01554018d1bd4bbb72cf94fc199
dohuyanevezuch <n.viktor119@mail.ru>
2026-07-19 18:37:12 +0700

    test

--- 14/14-02/files/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  netology_vm_web_name = "${var.school_name}-${var.vpc_name}-${var.platform_name}-${var.id_name_web}"
-  netology_vm_db_name = "${var.school_name}-${var.vpc_name}-${var.platform_name}-${var.id_name_db}"
-}
